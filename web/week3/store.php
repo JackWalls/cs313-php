7440dae7c8ce9b3f7f5b67d06469bce6c675c10a
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-11 16:20:50 -0700

    bugchecking

--- web/week3/store.php
+++ web/week3/store.php
@@ -19,36 +19,6 @@
 	<body>
 		<form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
 		<?php
-		$items = array (
-        'item1' => array (
-                'name' => 'French Macarons',
-                'desc' => 'Sweet meringue based cookies, comes in a dozen',
-                'price' => 14.99 
-        ),
-        'item2' => array (
-                'name' => 'Assorted Brownies',
-                'desc' => 'Assortment of brownies of different flavors, comes in a dozen',
-                'price' => 15.99
-        ),
-        'item3' => array (
-                'name' => 'Biscotti',
-                'desc' => 'Italian almond biscuits, comes in a pack of 15',
-                'price' => 9.99 
-        )
-	);
-        foreach($items as $ino => $item) {
-			
-            echo "$item['name']";
-			
-
-			if(in_array($ino, $_SESSION['cart'])) {
-				echo "Item already selected";
-            } 
-			else {
-                echo "<button type='submit' name='buy' value='$ino'>Buy</button> ";
-            }
-        }
-		var_dump($items);
 		echo "hello";
     ?>
 		</form>
