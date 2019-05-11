b6e9a09c6114be60b8216be5c57d19e86055e8e2
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-11 14:58:44 -0700

    redid store page and checkout page

--- web/week3/checkout.php
+++ web/week3/checkout.php
@@ -1,70 +1,75 @@
+<?php
+	session_start();
+	$items = array (
+        'item1' => array (
+                'name' => 'French Macarons',
+                'desc' => 'Sweet meringue based cookies, comes in a dozen',
+                'price' => 14.99 
+        ),
+        'item2' => array (
+                'name' => 'Assorted Brownies',
+                'desc' => 'Assortment of brownies of different flavors, comes in a dozen',
+                'price' => 15.99
+        ),
+        'item3' => array (
+                'name' => 'Biscotti',
+                'desc' => 'Italian almond biscuits, comes in a pack of 15',
+                'price' => 9.99 
+        )
+	);
+	if(!isset($_SESSION['cart'])) {
+		$_SESSION['cart'] = array();
+	}
+	
+	if (isset ( $_POST ['delete'] )) { 
+		if (false !== $key = array_search($_POST['delete'], $_SESSION['cart'])) {
+			unset($_SESSION['cart'][$key]);
+		}
+	}
+?>
+
 <DOCTYPE! html>
 <html>
 	<head>
 		<title>Results</title>
 	</head>
 	<body>
-<?php
-
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-
-  // User Details variables (feel free to adjust variable names where appropriate)
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $major = test_input($_POST["major"]);
-  $comments = test_input($_POST["comments"]);
-
-  // Displaying User Details
-  echo "Your name: " . $name . "\n";
-  echo "Your email: " . $email . "\n";
-  echo "Your major: " . $major . "\n";
-  echo "Your comments: " . $comments . "\n";
-
-  //Continents Variables
-  $northAmerica = test_input($_POST["na"]);
-  $southAmerica = test_input($_POST["sa"]);
-  $europe = test_input($_POST["eu"]);
-  $asia = test_input($_POST["as"]);
-  $australia = test_input($_POST["au"]);
-  $africa = test_input($_POST["af"]);
-  $antartica = test_input($_POST["an"]);
-
-  //Displaying Continents Visited
-
-  //if no continents are checked
-  if (empty($northAmerica) && empty($southAmerica) && empty($europe) && empty($asia) && empty($australia) && empty($africa) && empty($antartica)) {
-    echo "\n\nUser has not visited any continents :(\n";
-  } else {
-    echo "\n\nContinents Visited: \n"; //when at least one is checked
-  }
-
-  // displating each checked continent
-  if (!empty($northAmerica)) {
-    echo "North America\n";
-  }
-  if (!empty($southAmerica)) {
-    echo "South America\n";
-  }
-  if (!empty($europe)) {
-    echo "Europe\n";
-  }
-  if (!empty($asia)) {
-    echo "Asia\n";
-  }
-  if (!empty($australia)) {
-    echo "Australia\n";
-  }
-  if (!empty($africa)) {
-    echo "Africa\n";
-  }
-  if (!empty($antartica)) {
-    echo "Antarctica\n";
-  }
-?>
+		<?php
+			if (isset ( $_SESSION ["cart"] )) {
+		?>
+		<form action="" method="post">
+			<table>
+			<tr>
+				<th>Product</th>
+				<th>Price</th>
+				<th>Action</th>
+			</tr>
+			<?php
+				$total = 0;
+				foreach ( $_SESSION['cart'] as $ino ) {
+				?>
+					<tr>
+						<td> Name: <?php echo $items[$ino]['name']; ?></td>
+						<td> Price: <?php echo $items[$ino]["price"]; ?></td>
+						<td><button type='submit' name='delete' value='<?php echo $ino; ?>'>Remove</button></td>
+					</tr>
+					<?php
+						$total += $items[$ino]['price'];
+				}
+					?>
+			</table>
+			Total: $<?php echo $total; ?>
+			<input type="submit" value="Checkout">
+		</form>
+		<?php }
+		else {
+		?>
+		<p> You have not items in cart</p>
+		<?php }
+		?>
+		<form action="store.php>
+			<input type="submit" value="Go back to Browse" />
+		</form>
 	</body>
 </html>
+		
---
+++ web/week3/store.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+	$items = array (
+        'item1' => array (
+                'name' => 'French Macarons',
+                'desc' => 'Sweet meringue based cookies, comes in a dozen',
+                'price' => 14.99 
+        ),
+        'item2' => array (
+                'name' => 'Assorted Brownies',
+                'desc' => 'Assortment of brownies of different flavors, comes in a dozen',
+                'price' => 15.99
+        ),
+        'item3' => array (
+                'name' => 'Biscotti',
+                'desc' => 'Italian almond biscuits, comes in a pack of 15',
+                'price' => 9.99 
+        )
+	);
+	if(!isset($_SESSION['cart'])) {
+		$_SESSION['cart'] = array();
+	}
+	
+	if (isset ( $_POST ["buy"] )) {
+		if (!in_array($_POST ["buy"], $_SESSION['cart'])) {
+			$_SESSION ['cart'][] = $_POST["buy"];
+		}
+	}
+?>
+<DOCTYPE! html>
+<html>
+	<head>
+		<title>Store Page</title>
+		<link rel="stylesheet" type="text/css" href="storestyle.css"/>
+	</head>
+	<body>
+		<form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+    <?php
+        foreach ( $items as $ino => $item ) {
+			
+            echo "<p>$item['name']</p>";
+			echo "<p>$item['desc']</p>";
+			echo "<p>\$$item['price']</p>";
+
+			if (in_array($ino, $_SESSION['cart'])) {
+				echo "Item already selected";
+            } else {
+                echo "<button type='submit' name='buy' value='$ino'>Buy</button> ";
+            }
+        }
+    ?>
+</form>
+	<body>
+</html>
