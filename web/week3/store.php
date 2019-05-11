21688c48d2c631b392b97c14f14f9edd51d4543f
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-11 16:18:32 -0700

    bugchecking

--- web/week3/store.php
+++ web/week3/store.php
@@ -1,22 +1,6 @@
 <?php
 	session_start();
-	$items = array (
-        'item1' => array (
-                'name' => 'French Macarons',
-                'desc' => 'Sweet meringue based cookies, comes in a dozen',
-                'price' => 14.99 
-        ),
-        'item2' => array (
-                'name' => 'Assorted Brownies',
-                'desc' => 'Assortment of brownies of different flavors, comes in a dozen',
-                'price' => 15.99
-        ),
-        'item3' => array (
-                'name' => 'Biscotti',
-                'desc' => 'Italian almond biscuits, comes in a pack of 15',
-                'price' => 9.99 
-        )
-	);
+
 	if(!isset($_SESSION['cart'])) {
 		$_SESSION['cart'] = array();
 	}
@@ -35,6 +19,23 @@
 	<body>
 		<form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
 		<?php
+		$items = array (
+        'item1' => array (
+                'name' => 'French Macarons',
+                'desc' => 'Sweet meringue based cookies, comes in a dozen',
+                'price' => 14.99 
+        ),
+        'item2' => array (
+                'name' => 'Assorted Brownies',
+                'desc' => 'Assortment of brownies of different flavors, comes in a dozen',
+                'price' => 15.99
+        ),
+        'item3' => array (
+                'name' => 'Biscotti',
+                'desc' => 'Italian almond biscuits, comes in a pack of 15',
+                'price' => 9.99 
+        )
+	);
         foreach($items as $ino => $item) {
 			
             echo "<p>$item['name']</p>";
@@ -48,6 +49,7 @@
                 echo "<button type='submit' name='buy' value='$ino'>Buy</button> ";
             }
         }
+		var_dump($items);
     ?>
 		</form>
 	</body>
