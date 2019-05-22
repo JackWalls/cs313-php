22f5d9b9a6a29130ce9569130641acf91b2e756c
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-22 16:03:10 -0700

    Team activity 4

---
+++ web/week4/ta4.php
@@ -0,0 +1,37 @@
+<?php
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+
+		$dbOpts = parse_url($dbUrl);
+
+		$dbHost = $dbOpts["host"];
+		$dbPort = $dbOpts["port"];
+		$dbUser = $dbOpts["user"];
+		$dbPassword = $dbOpts["pass"];
+		$dbName = ltrim($dbOpts["path"],'/');
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex) {
+		echo 'Error!: ' . $ex->getMessage();
+		die();
+	}
+?>
+<DOCTYPE! html>
+<html>
+	<head>
+		<title>Team Activity 4</title>
+	</head>
+	<body>
+		<h1>Scripture Resources</h1>
+		<?php
+			foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row) {
+				echo "<p><span class='ref'>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span> - " . '"' . $row['content'] . '"' . "</p>";
+			}
+		?>
+	</body>
+</html>
+				
+		
