1c0a2832e780d36adec6f40da07e01205e253d7e
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-30 19:40:38 -0700

    Display test for client form

--- web/week4/clientForm.php
+++ web/week4/clientForm.php
@@ -1,5 +1,26 @@
 <?php
 	session_start();
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+
+		$dbOpts = parse_url($dbUrl);
+
+		$dbHost = $dbOpts["host"];
+		$dbPort = $dbOpts["port"];
+		$dbUser = $dbOpts["user"];
+		$dbPassword = $dbOpts["pass"];
+		$dbName = ltrim($dbOpts["path"],'/');
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		
+		$_SESSION['db'] = $db;
+
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex) {
+		echo 'Error!: ' . $ex->getMessage();
+		die();
+	}
 ?>
 <DOCTYPE! html>
 <html>
