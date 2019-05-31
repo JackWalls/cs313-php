81961823b8e93a017764102862f55b38484d290a
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-30 19:43:08 -0700

    Display test for client form

--- web/week4/clientForm.php
+++ web/week4/clientForm.php
@@ -24,7 +24,48 @@
 				</tr>
 			<?php
 				$id = $_POST['contractor'];
-				
+				$count = 9;
+				$half = false;
+				for ($i = 0; $i <= 16; $i++) {
+					$open = true;
+					foreach ($_SESSION['db']->query('SELECT time FROM occupy.time WHERE contractor_id="$id"') as $row) {
+						$check = explode(":", $row['time']);
+						if ($check[1] == 30) {
+							$halfCheck = true;
+						}
+						if ($count == $check[0] && $half == $halfCheck) {
+							$open = false;
+						}
+					}
+					if ($count > 12) {
+						$time = $count - 12;
+					}
+					else {
+						$time = $count;
+					}
+					if ($open == false) {
+						if ($half == true) {
+							$half = false;
+							echo "<tr><td id = 'closed'>" . $time . ":30</td></tr>";
+							$count = $count + 1;
+						}
+						else {
+							$half = true;
+							echo "<tr><td id = 'closed'>" . $time . ":00</td></tr>";
+						}
+					}
+					else {
+						if ($half == true) {
+							$half = false;
+							echo "<tr><td id = 'closed'><input type='radio' name='time' value='".$time.":30'>" . $time . ":30</td></tr>";
+							$count = $count + 1;
+						}
+						else {
+							$half = true;
+							echo "<tr><td id = 'closed'><input type='radio' name='time' value='".$time.":00'>" . $time . ":00</td></tr>";
+						}
+					}
+				}
 			?>
 			</table>
 			<input type="submit">
