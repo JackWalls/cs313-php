5aa3442743731bac91bd4c69d1327d104fc524b3
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-31 20:21:35 -0700

    Insert test

--- web/week4/insertCheck.php
+++ web/week4/insertCheck.php
@@ -46,10 +46,10 @@
 				$statement->bindvalue(':postal', $_POST['postal']);
 				$statement->bindvalue(':time', $_POST['time']);
 				$statement->bindvalue(':message', $_POST['message']);
-				echo "<p>Test</p>";		
+				//echo "<p>Test</p>";		
 				$statement->execute();
 						
-				echo "<p>Appointment registered</p>";
+				//echo "<p>Appointment registered</p>";
 			}
 			catch (Exception $ex) {
 				echo "<p>Appointment did not register" . $ex . " </p>";
