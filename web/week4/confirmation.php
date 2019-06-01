0080f3ed4599965b71718817cec575c3084c7d72
Jack Walls <josejoaquinparedes0@gmail.com>
2019-05-31 23:45:37 -0700

    add contractor test

--- web/week4/confirmation.php
+++ web/week4/confirmation.php
@@ -30,7 +30,7 @@
 	<body>
 		<?php
 			try {
-				$query = 'INSERT INTO occupy.contractor (name, state) 
+				$query = 'INSERT INTO occupy.contractor (contractor, state) 
 				VALUES(:name, :state)';
 					
 				$statement = $_SESSION['db']->prepare($query);
