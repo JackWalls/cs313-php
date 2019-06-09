8cbe4105b9ee810c05072fe78bf80eac3be45dd5
Jack Walls <josejoaquinparedes0@gmail.com>
2019-06-08 18:55:53 -0700

    Final database

--- web/db/myDB.sql
+++ web/db/myDB.sql
@@ -2,47 +2,30 @@ CREATE DATABASE scheduler;
 
 \c scheduler
 
-CREATE TABLE schedule.name {
+CREATE TABLE appoint (
 	id SERIAL PRIMARY KEY,
+	contractor_id INT NOT NULL REFERENCES occupy.contractor(id),
 	firstname varchar(100) NOT NULL,
-	lastname varchar(100) NOT NULL
-};
-
-CREATE TABLE schedule.contact {
-	id SERIAL PRIMARY KEY,
+	lastname varchar(100) NOT NULL,
 	telephone varchar(100) NOT NULL,
-	email varchar(150) NOT NULL UNIQUE
-};
-
-CREATE TABLE schedule.address {
-	id SERIAL PRIMARY KEY,
+	email varchar(150) NOT NULL UNIQUE,
 	street varchar(100) NOT NULL,
 	city varchar(100) NOT NULL,
 	state varchar(2) NOT NULL,
 	postal varchar(20) NOT NULL,
-};
-
-CREATE TABLE schedule.appoint {
-	id SERIAL PRIMARY KEY,
-	name_id INT NOT NULL REFERENCES schedule.name(id),
-	contact_id INT NOT NULL REFERENCES schedule.contact(id),
-	address_id INT NOT NULL REFERENCES schedule.address(id),
 	time varchar(20) NOT NULL,
 	message varchar(255) NOT NULL
-};
+);
 
-CREATE TABLE occupy.contractor {
+CREATE TABLE occupy.contractor (
 	id SERIAL PRIMARY KEY,
-	contractor varchar(100) NOT NULL
-};
-
-CREATE TABLE occupy.state {
-	id SERIAL PRIMARY KEY,
+	contractor varchar(100) NOT NULL,
 	state varchar(2) NOT NULL
-};
+);
 
-CREATE TABLE occupy.time {
+
+CREATE TABLE occupy.time (
 	id SERIAL PRIMARY KEY,
 	contractor_id INT NOT NULL REFERENCES occupy.contractor(id),
 	time varchar(20) NOT NULL
-};
+);
